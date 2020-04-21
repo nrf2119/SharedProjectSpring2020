fe6aad3f76fbe62d65a757acd5a75cabed5fb15e
ccchrischen6 <ccchrischen6@gmail.com>
2020-04-21 17:30:47 -0400

    commited on April 21th

--- app/src/main/java/com/example/android/sharedprojectspring2020/MainActivity.java
+++ app/src/main/java/com/example/android/sharedprojectspring2020/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //LOL! I am starting my final project!
+
+        //this is Chris Chen
     }
 }
